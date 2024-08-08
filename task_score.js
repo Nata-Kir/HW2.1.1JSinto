13ce29e07df884601f16cf3ac8fd482d9127ac86
NataZakharenka <natallia.kirsanava@gmail.com>
2024-08-08 17:38:43 +0300

    HT2.1.3 Added task_score file

---
+++ task_score.js
@@ -0,0 +1,20 @@
+
+let score1 = 10
+let score2 = 8
+let score3 = 7
+
+console.log(`введение ${score1}, git ${score2}, js ${score3}`);
+
+let result = score1 + score2 + score3
+
+console.log(result); 
+
+let averageResult = result/3
+
+console.log(Math.trunc(averageResult));
+
+let studentFirsName = "Natallia"
+let studentSecondName = "Zakharenka"
+
+console.log(`${studentFirsName} ${studentSecondName} average result for all modules is ${Math.trunc(averageResult)}`);
+
